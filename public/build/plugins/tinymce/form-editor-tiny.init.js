c760a9c83112a9f43ed3fc2c53d970799578d83d
repositapp <repositaplikasi@gmail.com>
2025-07-09 13:16:49 +0800

    menambah file build admin css

---
+++ public/build/plugins/tinymce/form-editor-tiny.init.js
@@ -0,0 +1,68 @@
+/*
+Template Name: Velzon - Admin & Dashboard Template
+Author: Themesbrand
+Website: https://Themesbrand.com/
+Contact: Themesbrand@gmail.com
+File: Form editor Js File
+*/
+
+tinymce
+var editor_config = {
+    path_absolute: "/",
+    selector: "textarea.tinymce",
+    relative_urls: false,
+    plugins: [
+        "advlist",
+        "autolink",
+        "lists",
+        "link",
+        "image",
+        "charmap",
+        "preview",
+        "anchor",
+        "searchreplace",
+        "visualblocks",
+        "code",
+        "fullscreen",
+        "insertdatetime",
+        "media",
+        "table",
+        "wordcount",
+    ],
+    toolbar:
+        "insertfile undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link",
+    file_picker_callback: function (callback, value, meta) {
+        var x =
+            window.innerWidth ||
+            document.documentElement.clientWidth ||
+            document.getElementsByTagName("body")[0].clientWidth;
+        var y =
+            window.innerHeight ||
+            document.documentElement.clientHeight ||
+            document.getElementsByTagName("body")[0].clientHeight;
+
+        var cmsURL =
+            editor_config.path_absolute +
+            "laravel-filemanager?editor=" +
+            meta.fieldname;
+        if (meta.filetype == "image") {
+            cmsURL = cmsURL + "&type=Images";
+        } else {
+            cmsURL = cmsURL + "&type=Files";
+        }
+
+        tinyMCE.activeEditor.windowManager.openUrl({
+            url: cmsURL,
+            title: "Filemanager",
+            width: x * 0.8,
+            height: y * 0.8,
+            resizable: "yes",
+            close_previous: "no",
+            onMessage: (api, message) => {
+                callback(message.content);
+            },
+        });
+    },
+};
+
+tinymce.init(editor_config);
